0ea802b278dea2b5a8146131485580493f5ca442
LSDZaddi <160318353+mordertehmod@users.noreply.github.com>
2026-02-23 18:47:15 -0600

    [Fix] - questify - Fix patch

--- src/plugins/questify/index.tsx
+++ src/plugins/questify/index.tsx
@@ -1007,6 +1007,8 @@ function getQuestAcceptedButtonText(quest: Quest, prepositional: boolean = false
 }
 
 function getQuestPanelPercentComplete({ quest, percentCompleteText }: { quest: Quest; percentCompleteText?: string; }): { percentComplete: number; } | { percentComplete: number; percentCompleteText: string; } | null {
+    if (!quest) { return null; }
+
     const task = getQuestTask(quest);
 
     if (!task) { return null; }
@@ -1164,8 +1166,6 @@ function getQuestAcceptedButtonProps(quest: Quest, text: string, disabled: boole
     };
 }
 
-// Drop support for QuestCompleter and migrate to Questify settings.
-migratePluginToSettings(true, "Questify", "QuestCompleter", "completeVideoQuestsInBackground", "completeGameQuestsInBackground", "completeAchievementQuestsInBackground");
 
 export default definePlugin({
     name: "Questify",
@@ -1294,8 +1294,8 @@ export default definePlugin({
             // Fixes the progress tracking for auto-completing Quests.
             find: ",{progressTextAnimation:",
             replacement: {
-                match: /(?<=children:\i}=)(\i)/,
-                replace: "Object.assign({},$1,$1.quest?$self.getQuestPanelPercentComplete($1):{})"
+                match: /(let{percentComplete:.{0,115}?children:\i}=)(\i)/,
+                replace: "const questifyProgress=$self.getQuestPanelPercentComplete({...$2,quest:$2.children?.props?.quest});$1Object.assign({},$2,questifyProgress??{})"
             }
         },
         {
