bacb01aac7d21a732305d503dee1ba5513460e5d
LSDZaddi <160318353+mordertehmod@users.noreply.github.com>
2026-03-24 11:34:15 +0000

    [Update] - ReviewDB - Update render function to use ErrorBoundary

--- src/plugins/reviewDB/index.tsx
+++ src/plugins/reviewDB/index.tsx
@@ -19,7 +19,6 @@
 import "./style.css";
 
 import { NavContextMenuPatchCallback } from "@api/ContextMenu";
-import ErrorBoundary from "@components/ErrorBoundary";
 import { OpenExternalIcon } from "@components/Icons";
 import { Paragraph } from "@components/Paragraph";
 import { Span } from "@components/Span";
@@ -71,6 +70,7 @@ export default definePlugin({
     name: "ReviewDB",
     description: "Review other users (Adds a new settings to profiles)",
     authors: [Devs.mantikafasi, Devs.Ven],
+    isModified: true,
 
     settings,
     contextMenus: {
@@ -80,25 +80,6 @@ export default definePlugin({
         "user-profile-actions": userContextPatch,
         "user-profile-overflow-menu": userContextPatch
     },
-
-    patches: [
-        {
-            // DM profile sidebar
-            find: ".SIDEBAR,disableToolbar:",
-            replacement: {
-                match: /user:(\i),widgets:.{0,100}?\}\),/,
-                replace: "$&$self.renderProfileComponent({user:$1,isSideBar:true}),"
-            }
-        },
-        {
-            // User popout
-            find: /\.POPOUT,onClose:\i}\),nicknameIcons:.+?\.isProvisional/,
-            replacement: {
-                match: /user:(\i),widgets:.{0,100}?\}\),/,
-                replace: "$&$self.renderProfileComponent({user:$1}),"
-            }
-        }
-    ],
 
     flux: {
         CONNECTION_OPEN: initAuth,
@@ -154,7 +135,7 @@ export default definePlugin({
         }, 4000);
     },
 
-    renderProfileComponent: ErrorBoundary.wrap(({ user, isSideBar = false }: { user: User; isSideBar?: boolean; }) => {
+    renderProfileCollection: ({ user, isSideBar = false }: { user: User; isSideBar?: boolean; }) => {
         const [reviewData] = useAwaiter(() => getReviews(user.id, { limit: 4 }), { deps: [user.id], fallbackValue: null });
 
         // Discord are masters at using a crap ton of html elements and css classes to create a simple ui that could have
@@ -190,7 +171,7 @@ export default definePlugin({
                                                             {showCount && (
                                                                 <div className={ProfileCardContainerClasses.displayCountText}>
                                                                     <Span className={ProfileCardContainerClasses.displayCountTextColor} size="xs" weight="medium" defaultColor={false}>
-                                                                        +{reviewData.reviewCount - 3}
+                                                                        +{reviewData.reviewCount - 4}
                                                                     </Span>
                                                                 </div>
                                                             )}
@@ -211,5 +192,5 @@ export default definePlugin({
         return isSideBar
             ? <div className={DMSideBarClasses.widgetPreviews}>{reviewsSection}</div>
             : reviewsSection;
-    }, { noop: true })
+    }
 });
